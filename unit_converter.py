c309577ed3f193e191ee6a8b60b0aa23985a2884
ianjunyining <ianjunyining@users.noreply.github.com>
2023-04-30 20:38:54 -0700

    Weight converter formating

--- unit_converter.py
+++ unit_converter.py
@@ -55,7 +55,6 @@ if Unit == "Length":
             options=length_dict.keys(),
         )
         result = convert(length_dict, from_select, to_select, from_input)  
-        # result = convert_length(from_select, to_select, from_input)
         to_input = st.number_input(
             "to_input", 
             label_visibility="collapsed", 
@@ -63,22 +62,67 @@ if Unit == "Length":
         )
 
 elif Unit == "Weight":
-    c1, c2, c3 = st.columns(3)
     st.title("Weight Unit Converter")
+    c1, c2, c3 = st.columns(3)
+    weight_dict = {
+        "Kilogram" : 1,
+        "Pound" : 0.45359237,
+        "Ounce" : 0.028349523,
+        "Milligram" : 1e-6,
+        "Microgram" : 1e-9,
+        "Gram" : 0.001,
+        "Ton" : 1000,
+    }
     with c1:
-        from_input = st.number_input("Value:")
-        from_select = st.selectbox(label_visibility="collapsed", options=("Kilogram", "Pound", "Ounce", "Milligram", "Microgram", "Gram", "Ton"), label="")
+        from_select = st.selectbox(
+            label_visibility="collapsed", 
+            options=weight_dict.keys(), 
+            label="from_select",
+        )
+        from_input = st.number_input(
+            "from_input", 
+            label_visibility="collapsed",
+        )
     with c2:
-        st.markdown("<h1 style='font-size: 80px; text-align: center;'>=</h1>", unsafe_allow_html=True)
+        st.markdown(
+            "<h1 style='font-size: 80px; text-align: center;'>=</h1>",
+            unsafe_allow_html=True,
+        )
     with c3:
-        to_select = st.selectbox("   ", ("Kilogram", "Pound", "Ounce", "Milligram", "Microgram", "Gram", "Ton"), label_visibility="collapsed")
-        result = convert_weight(from_select, to_select, from_input)
-        st.text(result)
+        to_select = st.selectbox(
+            "to_select", 
+            options=weight_dict.keys(), 
+            label_visibility="collapsed",
+        )
+        result = convert(weight_dict, from_select, to_select, from_input)
+        to_input = st.number_input(
+            "to_input", 
+            label_visibility="collapsed", 
+            value=result,
+        )
+
 elif Unit == "Credits and Notes":
     st.title("Unit Converter - Credits")
-    st.markdown("""<h5>Thanks for Huazhong for teaching me how to use streamlit</h5>
-    <h5>Thanks for Ian(Me) for creating the code</h2>
-    <h5>Thanks for Hengqingchu for viewing my project</h5>
-    <h2>Notes</h2>
-    <p>    If you are viewing this Unit Converter I would thank you for doing that. Me as the the creator when I first saw this I had fun with it and now I am a expert at it. I learned streamlit in 1 day ( Almost all the functions the python library has ). I had lots of fun coding this thing and I will make another project in the future. When I learned streamlit it was kind of easy but now learning after 1 day it became easier than the first time I coded with it. I discovered function after function and now I almost know the whole library. I also learned that it is a data based library to make websites and that you can't make clicker games or some other games. I really like this project because converting length, speed, and weight is hard but I did it.</p>""", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <h5>Thanks for Huazhong for teaching me how to use streamlit</h5>
+        <h5>Thanks for Ian(Me) for creating the code</h2>
+        <h5>Thanks for Hengqingchu for viewing my project</h5>
+        <h2>Notes</h2>
+        <p> If you are viewing this Unit Converter I would thank you 
+        for doing that. Me as the the creator when I first saw this I 
+        had fun with it and now I am a expert at it. I learned streamlit 
+        in 1 day ( Almost all the functions the python library has ). 
+        I had lots of fun coding this thing and I will make another 
+        project in the future. When I learned streamlit it was kind of 
+        easy but now learning after 1 day it became easier than the 
+        first time I coded with it. I discovered function after 
+        function and now I almost know the whole library. I also 
+        learned that it is a data based library to make websites and 
+        that you can't make clicker games or some other games. I 
+        really like this project because converting length, speed, 
+        and weight is hard but I did it.</p>
+        """, 
+        unsafe_allow_html=True,
+    )
     st.image(scr, width=70)
