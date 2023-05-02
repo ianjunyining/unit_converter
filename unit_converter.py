12c359a3d739f0bd6f0fccaefad130faeee8a874
ianjunyining <ianjunyining@users.noreply.github.com>
2023-05-01 20:40:54 -0700

    Update unit_converter.py
    Added BMI calculator and time Unit

--- unit_converter.py
+++ unit_converter.py
@@ -20,7 +20,7 @@ def convert_weight(from_select, to_select, input):
     return convert(weight_dict, from_select, to_select, input)
 
 
-Unit = st.sidebar.selectbox("Opitions: ", ("Length", "Weight", "Speed", "Credits and Notes"))
+Unit = st.sidebar.selectbox("Opitions: ", ("Length", "Weight", "Time", "BMI", "Credits and Notes"))
 if Unit == "Length":
     st.title("Length Unit Converter")
     length_dict = {
@@ -100,7 +100,113 @@ elif Unit == "Weight":
             label_visibility="collapsed", 
             value=result,
         )
+elif Unit == "Time":
+    st.title("Time Unit Converter")
+    c1, c2, c3 = st.columns(3)
+    time_dict = {
+        "Second" : 0.00001157407,
+        "Minute" : 0.00069444444,
+        "Hour" : 0.04166666666,
+        "Day" : 1,
+        "Week" : 7,
+        "Month" : 31,
+        "Year" : 365,
+        "Decade" : 3650,
+        "Century" : 36500,
+    }
+    with c1:
+        from_select = st.selectbox(
+            label_visibility="collapsed", 
+            options=time_dict.keys(), 
+            label="from_select",
+        )
+        from_input = st.number_input(
+            "from_input", 
+            label_visibility="collapsed",
+        )
+    with c2:
+        st.markdown(
+            "<h1 style='font-size: 80px; text-align: center;'>=</h1>",
+            unsafe_allow_html=True,
+        )
+    with c3:
+        to_select = st.selectbox(
+            "to_select", 
+            options=time_dict.keys(), 
+            label_visibility="collapsed",
+        )
+        result = convert(time_dict, from_select, to_select, from_input)
+        to_input = st.number_input(
+            "to_input", 
+            label_visibility="collapsed", 
+            value=result,
+        )
+elif Unit == "BMI":
+    st.title("BMI calculator")
+    c1, c2, c3 = st.columns(3)
+    weight_dict = {
+        "Kilogram" : 1,
+        "Pound" : 0.45359237,
+        "Ounce" : 0.028349523,
+        "Milligram" : 1e-6,
+        "Microgram" : 1e-9,
+        "Gram" : 0.001,
+        "Ton" : 1000,
+    }
+    length_dict = {
+        "Meter" : 1,
+        "Centimeter" : 0.01,
+        "Feet" : 0.3048,
+        "Inches" : 0.0254,
+        "Millimeter" : 0.001,
+        "Micrometer" : 0.000001,
+        "Mile" : 1609.344,
+    }
+    with c1:
+        st.markdown("<h1 style='font-size: 20px;'>weight</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='font-size: 20px;'>height</h1>", unsafe_allow_html=True)
+    with c2:
+        weight = st.number_input("weight", label_visibility="collapsed")
+        height = st.number_input("height", label_visibility="collapsed")
+    with c3:
+        weight_unit = st.selectbox(
+            "weight_unit", 
+            options=weight_dict.keys(), 
+            label_visibility="collapsed",
+        )
+        height_unit = st.selectbox(
+            label="height_unit",
+            label_visibility="collapsed",
+            options=length_dict.keys(),
+        )
+        kilograms = convert(weight_dict, weight_unit, "Kilogram", weight)
+        meter = convert(length_dict, height_unit, "Meter", height)
+        try:
+            bmi = kilograms / (meter ** 2)
+        except:
+            bmi = 0
 
+        if bmi < 19.9:
+            category = "underweight"
+        if bmi > 24.9:
+            category = "normal"
+        if bmi > 29.9:
+            category = "overweight"
+        if bmi > 34.9:
+            category = "Obesity I"
+        if bmi > 39.9:
+            category = "Obesity II"
+        if bmi > 44.9:
+            category = "Obesity Max"
+        else:
+            if bmi > 0:
+                category = "underweight"
+            else:
+                category = "None" 
+    if True:
+        st.text(f"Your BMI is {bmi} and is considered as {category}")
+    
+        pass
 elif Unit == "Credits and Notes":
     st.title("Unit Converter - Credits")
     st.markdown(
